6befdc7b5ffc707f7162e47aac0a1ea386e17ea9
HunterM <zakaspro@gmail.com>
2022-09-18 13:05:15 +0300

    дз 6 решение первой задачи

--- HWC#6/Program.cs
+++ HWC#6/Program.cs
@@ -2,36 +2,19 @@
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
-// Console.WriteLine("Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.");
-// Console.WriteLine();
-// int size = 20;
-// // int[] array = new int[size];
-// int count = 0;
-// // for (int i = 0; i < size; i++)
-// int number = 0;
-// Console.WriteLine("Введите отрицательное или положительное число M");
-// //int [] M = Array.ConvertAll(Console.ReadLine().Split());
-// number = Convert.ToInt32(m);
-// for (int i = 0; i < size; i++)
-// {
-//     M[i] = new Random().Next(-100, 100);
-//     Console.Write(M[i] + "\t");
-//     if (M[i] > 0) count++;
-// }
-// Console.WriteLine($"{"\n"}Количество четных чисел в массиве:  {count}");
-// Console.WriteLine();
-
-////////////////////////////
-
-// int size = Convert.ToInt32(Console.ReadLine());
-
-// int[] array = new int[size];
-// for (int i = 0; i < size; i++)
-// {
-//     array[i] = new Random().Next(100);
-//     Console.Write(array[i] + "\t");
-// }
-
+Console.Write("Введите элементы(через пробел): ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+Console.WriteLine($"Кол-во элементов больше 0: {count}");
 
 // Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
 //заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
