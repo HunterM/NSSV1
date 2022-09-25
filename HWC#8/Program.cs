d114b9040997a07db3a1b7ea8e4d54d62db0787a
HunterM <zakaspro@gmail.com>
2022-09-25 21:38:08 +0300

    ДЗ 7 промежуточное

--- HWC#8/Program.cs
+++ HWC#8/Program.cs
@@ -8,69 +8,65 @@
 // 9 5 3 2
 // 8 4 4 2
 
+// Console.WriteLine("Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.");
+// Console.WriteLine();
+// Console.WriteLine("Задайте количество строк двумерного массива:");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Задайте количество столбцов двумерного массива:");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int[,] twoDimArray = new int[m, n];
 
+// Random rnd = new Random();
+// void PrintArray(int[,] matr) //Вывод на печать массива
+// {
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             Console.Write($"{matr[i, j]} " + "\t");
+//         }
+//         Console.WriteLine();
+//     }
+// }
 
+// void FillArray(int[,] matr) //Заполнение  массива
+// {
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             matr[i, j] = Convert.ToInt32(rnd.Next(0, 100));
+//         }
+//     }
+// }
 
-Console.WriteLine("Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.");
-Console.WriteLine();
-Console.WriteLine("Задайте количество строк двумерного массива:");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Задайте количество столбцов двумерного массива:");
-int n = Convert.ToInt32(Console.ReadLine());
-int[,] twoDimArray = new int[m, n];
+// void NewArray(int[,] twoDimArray)  // Метод сортировки массива
+// {
+//     for (int i = 0; i < twoDimArray.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < twoDimArray.GetLength(1); j++)
+//         {
+//             for (int k = 0; k < twoDimArray.GetLength(1) - 1; k++)
+//             {
+//                 if (twoDimArray[i, k] < twoDimArray[i, k + 1])
+//                 {
+//                     int t = twoDimArray[i, k + 1];
+//                     twoDimArray[i, k + 1] = twoDimArray[i, k];
+//                     twoDimArray[i, k] = t;
+//                 }
+//             }
+//         }
+//     }
+// }
 
-Random rnd = new Random();
-void PrintArray(int[,] matr)
-{
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            Console.Write($"{matr[i, j]} " + "\t");
-        }
-        Console.WriteLine();
-    }
-}
+// FillArray(twoDimArray);
+// Console.WriteLine();
+// PrintArray(twoDimArray);
+// Console.WriteLine();
 
-void FillArray(int[,] matr)
-{
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            matr[i, j] = Convert.ToInt32(rnd.Next(0, 100));
-        }
-    }
-}
-
-void NewArray(int[,] twoDimArray)
-{
-  for (int i = 0; i < twoDimArray.GetLength(0); i++)
-  {
-    for (int j = 0; j < twoDimArray.GetLength(1); j++)
-    {
-      for (int k = 0; k < twoDimArray.GetLength(1) - 1; k++)
-      {
-        if (twoDimArray[i, k] < twoDimArray[i, k + 1])
-        {
-        int t = twoDimArray[i, k + 1];
-        twoDimArray[i, k + 1] = twoDimArray[i, k];
-        twoDimArray[i, k] = t;
-        }
-      }
-    }
-  }
-}
-
-
-FillArray(twoDimArray);
-Console.WriteLine();
-PrintArray(twoDimArray);
-Console.WriteLine();
-
-Console.WriteLine($"\n"+"Отсортированный массив: ");
-NewArray(twoDimArray);
-PrintArray(twoDimArray);
+// Console.WriteLine($"\n" + "Отсортированный массив: ");
+// NewArray(twoDimArray);
+// PrintArray(twoDimArray);
 
 
 // Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
@@ -81,7 +77,68 @@ PrintArray(twoDimArray);
 // 5 2 6 7
 // Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
 
+Console.WriteLine("Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.");
+Console.WriteLine();
+Console.WriteLine("Задайте количество строк двумерного массива:");
+int strok = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте количество столбцов двумерного массива:");
+int stolb = Convert.ToInt32(Console.ReadLine());
+int[,] massiv = new int[strok, stolb];
+Random mas = new Random();
 
+void PrintMassiv(int[,] massiv)
+{
+    for (int i = 0; i < strok; i++)
+    {
+        for (int j = 0; j < stolb; j++)
+        {
+            Console.Write($"{massiv[i, j]} " + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillMassiv(int[,] massiv)
+{
+    for (int i = 0; i < strok; i++)
+    {
+        for (int j = 0; j < stolb; j++)
+        {
+            massiv[i, j] = Convert.ToInt32(mas.Next(0, 100));
+        }
+    }
+}
+void SumStr(int[,] massiv)
+{
+    int sum = 0;
+    int index = 0;
+    for (int i = 0; i < massiv.GetLength(1); i++)
+        {
+            int temp = 0;
+            for (int j = 0; j < massiv.GetLength(0); j++)
+                {
+                    temp += massiv[j, i];
+                }
+                    if (temp > sum)
+                        {
+                            sum = temp;
+                            index = i;
+                        }
+        }
+Console.WriteLine("Столбец: " );
+for (int i = 0; i < massiv.GetLength(0); i++)
+   {
+        Console.WriteLine(massiv[i, index]);
+    }
+}
+
+FillMassiv(massiv);
+Console.WriteLine();
+PrintMassiv(massiv);
+Console.WriteLine();
+
+// PrintMassiv(massiv);
+SumStr(massiv);
 
 // Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
 // Например, даны 2 матрицы:
@@ -93,7 +150,8 @@ PrintArray(twoDimArray);
 
 
 
-// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+//Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
 // Массив размером 2 x 2 x 2
 // 66(0,0,0) 25(0,1,0)
 // 34(1,0,0) 41(1,1,0)
