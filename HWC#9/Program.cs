042e9db9b12955a02a3e11628275450559f4681d
HunterM <zakaspro@gmail.com>
2022-09-30 19:55:05 +0300

    ДЗ 9 сохранялка 1

---
+++ HWC#9/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+// Console.Write("Введите число N: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// if (1 > N)
+// {
+//     Console.WriteLine("Это невозможно");
+//     return;
+// }
+// string PrintNumbers(int start, int end)
+// {
+//     if (start == end) return start.ToString();
+//     return (start + ", " + PrintNumbers(start + 1, end));
+// }
+// Console.WriteLine(PrintNumbers(1, N));
+
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int InputNumber(string input)
+{
+     Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+ }
+
+ void PrintSum(int m, int n, int sum)
+ {
+     sum = sum + n;
+     if (n <= m)
+    {
+         Console.Write($" {sum} ");
+         return;
+     }
+     PrintSum(m, n - 1, sum);
+
+ }
+ int m = InputNumber("Введите число М: ");
+ int n = InputNumber("Введите число N: ");
+ int temp = m;
+
+ if (m > n)
+ {
+     m = n;
+     n = temp;
+ }
+ Console.Write($"{m}, {n} ->");
+ PrintSum(m, n, temp = 0);
+
+
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
